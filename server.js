cbc428f35898978d12439c908e63f61925a53263
SMCS2024AZ <dnkmanager@gmail.com>
2022-04-06 17:32:12 -0400

    Stopwatch setup

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require("express")
 const app = express()
 
+app.set("view engine", "ejs")
+
+const stopwatch = require("./routes/stopwatch")
+app.use("/stopwatch", stopwatch)
+
 app.listen(8080)
