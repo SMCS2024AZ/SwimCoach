e4662c9abbd7d02b9c029f6bb5015362664cf60b
SMCS2024AZ <dnkmanager@gmail.com>
2022-04-09 10:40:22 -0400

    Database prod vs local

--- db/index.js
+++ db/index.js
@@ -1,14 +1,23 @@
 const { Client } = require("pg");
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL || process.env.LOCAL_DB,
-  ssl: process.env.DATABASE_URL ? true : false
+const client = new Client(() => {
+  if (process.env.NODE_ENV != "production") {
+    return {
+      connectionString: process.env.LOCAL_DB,
+      ssl: false
+    };
+  } else {
+    return  {
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    };
+  }
 });
 
 client.connect();
 
 module.exports = {
   query: (text, params, callback) => {
-    return client.query(text, params, callback)
+    return client.query(text, params, callback);
   },
 };
