be8627105383e54ebfea44f4032316aff5c4cf74
SMCS2024AZ <dnkmanager@gmail.com>
2022-04-09 10:48:39 -0400

    testing database error handling

--- routes/stopwatch.js
+++ routes/stopwatch.js
@@ -6,7 +6,6 @@ router.get("/", (req, res) => {
   db.query("SELECT * FROM swimmers WHERE age_group = '10 and under'",
   [],
   (err, result) => {
-    if (err) return err;
     res.render("setup", { swimmers : result.rows });
   });
 });
@@ -15,7 +14,6 @@ router.post("/", (req, res) => {
   db.query("SELECT * FROM swimmers WHERE age_group = $1",
   [req.body.group],
   (err, result) => {
-    if (err) return err;
     res.send(result.rows);
   });
 });
