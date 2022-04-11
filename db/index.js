232d46912bb7c0a253ce7acab756e15fd21d5467
SMCS2024AZ <dnkmanager@gmail.com>
2022-04-10 23:17:37 -0400

    testing prod

--- db/index.js
+++ db/index.js
@@ -1,16 +1,17 @@
 const { Client } = require("pg");
 
-/*-----Only for prod-----
+/*-----Only for prod-----*/
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: { rejectUnauthorized: false }
 });
-*/
 
+/*
 const client = new Client({
     connectionString: process.env.LOCAL_DB,
     ssl: false
 });
+*/
 
 client.connect();
 
