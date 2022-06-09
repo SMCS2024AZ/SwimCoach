a9ed3d3c6c64c8c9185a6b7407f896abd40c2342
SMCS2024AZ <dnkmanager@gmail.com>
2022-06-09 00:48:21 -0400

    Fixed gender dropdown

--- public/scripts/manager.js
+++ public/scripts/manager.js
@@ -3,12 +3,16 @@ $(document).ready(function() {
   var swimmerTable = $("#swimmerTable").DataTable({
     info: false,
     lengthChange: false,
-    ordering: false,
+    order: [[0, "asc"]],
     pageLength: 8,
     columnDefs: [
       {
         target: 3,
         visible: false
+      },
+      {
+        targets: "_all",
+        orderable: false
       }
     ],
     initComplete: function() {
@@ -39,6 +43,10 @@ $(document).ready(function() {
     $(this).attr("class", "dropdown-item active " + type);
 
     updateSelections();
+  });
+
+  $(document).on("click", ".swimmerGender", function() {
+    $(".selectedGender").text($(".active.swimmerGender").text());
   });
 
   $("#searchbar").on("input", function() {
@@ -74,8 +82,29 @@ $(document).ready(function() {
     curr.children("td").each(function(index) {
       if (index < 3) {
         var text = $(this).text();
-        $(this).html(`<input type="hidden" value="${text}"><input type="text" class="form-control" value="${text}">`);
-        $(this).children().eq(1).css("width", text.length * 8 + 48);
+        if (index == 1) {
+          $(this).html(
+          `<div class="dropdown px-1">
+            <button type="button" class="btn btn-primary dropdown-toggle selectedGender" data-bs-toggle="dropdown">
+              ${text}
+            </button>
+            <input type="hidden" value="${text}">
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item swimmerGender">Male</a></li>
+              <li><a class="dropdown-item swimmerGender">Female</a></li>
+              <li><a class="dropdown-item swimmerGender">Other</a></li>
+            </ul>
+          </div>`);
+          $(this).children().eq(0).children().eq(2).children().each(function(index) {
+            //console.log($(this).text())
+            if ($(this).text() == text.trim()) {
+              $(this).children().eq(0).attr("class", "dropdown-item active swimmerGender");
+            }
+          });
+        } else {
+          $(this).html(`<input type="hidden" value="${text}"><input type="text" class="form-control" value="${text}">`);
+          $(this).children().eq(1).css("width", text.length * 8 + 48);
+        }
       }
     });
   });
@@ -90,7 +119,7 @@ $(document).ready(function() {
       type: "POST",
       data: JSON.stringify({
         name: $(this).closest("td").prevAll().eq(2).children().eq(1).val(),
-        gender: $(this).closest("td").prevAll().eq(1).children().eq(1).val(),
+        gender: $(this).closest("td").prevAll().eq(1).children().eq(0).children().eq(0).text(),
         age: parseInt($(this).closest("td").prevAll().eq(0).children().eq(1).val()),
         id: parseInt($(this).closest("td").prevAll("input").val())
       }),
@@ -99,7 +128,12 @@ $(document).ready(function() {
       success: function(data) {
           curr.children("td").each(function(index) {
           if (index < 3) {
-            var content = $(this).first().children("input").eq(1).val();
+            var content = "";
+            if (index == 1) {
+              content = $(this).children().eq(0).children().eq(0).text();
+            } else {
+              content = $(this).first().children("input").eq(1).val();
+            }
             $(this).html(content);
             $(this).css("width", "auto");
             swimmerTable.cell(this).data(content);
@@ -118,8 +152,12 @@ $(document).ready(function() {
     var curr = $(this).closest("tr");
     curr.children("td").each(function(index) {
       if (index < 3) {
-        $(this).html($(this).first().children("input").val())
-        $(this).css("width", "auto");
+        if (index == 1) {
+          $(this).html($(this).children().eq(0).children().eq(1).val());
+        } else {
+          $(this).html($(this).first().children("input").val());
+          $(this).css("width", "auto");
+        }
       }
     });
   });
