41ca28dd1898315ce9eea6162c64bafe614bb44e
SMCS2024AZ <dnkmanager@gmail.com>
2022-06-11 19:47:11 -0400

    Database

--- db/index.js
+++ db/index.js
@@ -5,10 +5,11 @@ const client = new Client({
     ssl: { rejectUnauthorized: false }
 });
 
+/*
 const client = new Client({
     connectionString: process.env.LOCAL_DB,
     ssl: false
-});
+});*/
 
 client.connect();
 
